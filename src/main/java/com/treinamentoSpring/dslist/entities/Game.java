fc3c35d7180315a1e887f0373b71da2467b4d776
wmelis85a <wmelis.85@gmail.com>
2025-05-22 23:13:17 -0300

    feat(DEV): adds Gameseed

--- src/main/java/com/treinamentoSpring/dslist/entities/Game.java
+++ src/main/java/com/treinamentoSpring/dslist/entities/Game.java
@@ -5,6 +5,7 @@ import java.util.Objects;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
@@ -13,27 +14,33 @@ import jakarta.persistence.Table;
 public class Game {
 	
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String title;
 	
 	@Column (name = "game_year")
 	private Integer year;
 	private String genre;
-	private String platform;
+	private String platforms;
+	private Double score;
 	private String imgUrl;
+	
+	@Column (columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column (columnDefinition = "TEXT")
 	private String longDescription;
 	
 
 
-	public Game(Long id, String title, Integer year, String genre, String platform, String imgUrl,
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
 			String shortDescription, String longDescription) {
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.platform = platform;
+		this.platforms = platforms;
+		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
@@ -90,13 +97,13 @@ public class Game {
 
 
 	public String getPlatform() {
-		return platform;
+		return platforms;
 	}
 
 
 
 	public void setPlatform(String platform) {
-		this.platform = platform;
+		this.platforms = platform;
 	}
 
 
@@ -140,6 +147,18 @@ public class Game {
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
+	}
+
+
+
+	public Double getScore() {
+		return score;
+	}
+
+
+
+	public void setScore(Double score) {
+		this.score = score;
 	}
 
 
